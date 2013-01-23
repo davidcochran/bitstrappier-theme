38cc46093f20bd269dc4c97293c9fbfb3aba8545
David Cochran <dc@Cochran-Air.local>
2013-01-22 21:43:30 -0600

    Tidying up, Removed _wp.less, Improved _app.less
    Removed _wp.less, Improved _app.less, some basic responsive adjustments
    for sidebar search form

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ require_once locate_template('/lib/utils.php');           // Utility functions
 require_once locate_template('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template('/lib/sidebar.php');         // Sidebar class
 require_once locate_template('/lib/config.php');          // Configuration
-require_once locate_template('/lib/activation.php');      // Theme activation
+// BitStrappier bypasses these activation options
+// require_once locate_template('/lib/activation.php');      // Theme activation
 require_once locate_template('/lib/cleanup.php');         // Cleanup
 require_once locate_template('/lib/nav.php');             // Custom nav modifications
 require_once locate_template('/lib/htaccess.php');        // Rewrites for assets, H5BP .htaccess
@@ -17,8 +18,11 @@ require_once locate_template('/lib/widgets.php');         // Sidebars and widget
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
 
-// To check and compile LESS in PHP!
-// NOTE: Plugins utilizing lessphp could call
-// for another instance of lessc, creating a conflict
-// Comment out to turn this functionality off in the theme
+// Lessphp
+// Check for LESS file updates and compile live in PHP
+//
+// NOTE: 	Plugins utilizing lessphp could call
+// 				for another instance of lessc, creating a conflict
+//				**Comment out to turn off this functionality in the theme**
+//
 require_once locate_template( '/lib/less.php' );					// Less Compiling functions
