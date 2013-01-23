470c1af16619aee08ccccdfa53e4c842ef599fc2
David Cochran <dc@cochran-air.att.net>
2013-01-23 12:23:37 -0600

    Tidying and commenting _app.less and less.php

--- lib/less.php
+++ lib/less.php
@@ -13,11 +13,22 @@ function bitstrappier_phpless( $inputFile, $outputFile ) {
 
   // For now lessphp runs all the time,
   // as long as its included in functions.php
-  // We could set up a condition to check.
+
+  // We could set up conditions and options such as:
   // if ( get_option( 'bitstrappier_minimize_css' ) == 1 ) {
+  // But we'll just leave it on and use comments to do things like:
+
+  // Minimize CSS when LESS is compiled
     $less->setFormatter( "compressed" );
+
+  // Preserve CSS Comments
     // $less->setPreserveComments(true); // uncomment this line to preserve comments
+
+  // Unneeded unless we setup a condition above
   // }
+
+// This creates a cache file to check all imported LESS files for updates
+// See http://leafo.net/lessphp/docs/#compiling_automatically
 
   // create a new cache object, and compile
   $cache = $less -> cachedCompile( $inputFile );
